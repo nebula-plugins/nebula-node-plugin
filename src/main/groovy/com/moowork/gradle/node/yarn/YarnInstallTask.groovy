ad6fcc4718c34eebb039c72b269c334cd9d9bd2d
Martin Chalupa <mchalupa@netflix.com>
2021-02-18 12:46:47 -0800

    Gradle 7.0 compatibility

--- src/main/groovy/com/moowork/gradle/node/yarn/YarnInstallTask.groovy
+++ src/main/groovy/com/moowork/gradle/node/yarn/YarnInstallTask.groovy
@@ -16,6 +16,7 @@ class YarnInstallTask
 
     public YarnInstallTask()
     {
+        super(true)
         this.group = 'Node'
         this.description = 'Install node packages using Yarn.'
         setYarnCommand( '' )
